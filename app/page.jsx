4e62a1af1adc25a86ffccd6b0f016b00727cbcd9
AzizZeynalli <a.zeynalli1@ufaz.az>
2023-09-25 17:07:35 +0400

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <section className="w-full flex justify-center flex-col">
+            <h1>Empowering AI Innovation Together ///</h1>
+        </section>
+    );
+}
+
+export default Home
